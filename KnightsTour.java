d144f62135d45bd6f2d1109ac29c4fd53a0a62a1
iykegoesmoo <iykecow@gmail.com>
2025-03-16 18:34:17 -0700

    bugfix

--- KnightsTour.java
+++ KnightsTour.java
@@ -3,19 +3,55 @@ public class KnightsTour {
     private int [][] board;
     private int counter = 0;
     private int stuck = 0;
+    private int finish = 0;
+
+    private int xDim = 0;
+    private int yDim = 0;
 
     private void function(int [][] arr, int a, int x, int y, int left, int up) {
-        if (a==0) {
-            if (arr[x-+left][y+up]==10) {
-                stuck++;
-                return;
-            }
-            else {
-                counter++;
-                arr[x][y]++;
-                arr[x+left][y+up]=9;
-                new KnightsTour(arr);
-            }
+
+        // System.out.println("COUNTER IS " + counter);
+        // System.out.println("xDim IS " + xDim);
+        // System.out.println("yDim IS " + yDim);
+
+        if (counter==(xDim*yDim)-1) {
+            System.out.println("YES");
+            finish++;
+            return;
+        }
+
+        if (x+left>xDim-1) {
+            counter++;
+            stuck++;
+            return;
+        }
+        else if (y+up>yDim-1) {
+            counter++;
+            stuck++;
+            return;
+        }
+        else if (y+up<0) {
+            counter++;
+            stuck++;
+            return;
+        }
+        else if (x+left<0) {
+            counter++;
+            stuck++;
+            return;
+        }
+
+        else if (arr[x+left][y+up]==10) {
+            counter++;
+            stuck++;
+            return;
+        }
+        else {
+            counter++;
+            arr[x][y]++;
+            arr[x+left][y+up]=9;
+            // System.out.println("Hello World");
+            new KnightsTour(arr);
         }
     }
 
@@ -23,10 +59,14 @@ public class KnightsTour {
     {
         board = data;
 
+        xDim = board.length;
+        yDim = board[0].length;
+
+
         // Notes: 
-        //  0 = Knight can't go to this square
-        //  9 = Knight
-        //  All other numbers are square the knight CAN go to.
+        //  0  = Square Knight hasn't gone to yet
+        //  9  = Knight square
+        //  10 = Already went to this square
         //  
 
         if (counter==0) {
@@ -62,35 +102,24 @@ public class KnightsTour {
                         if (k==7) {
                             function(data,k,i,j,-1,-3);
                         }
+
+                        // System.out.println("DONE");
                     }
-
-                    data[i+3][j-1]=1;
-                    data[i+3][j-1]=1;
-
-                    data[i+3][j+1]=1;
-                    data[i-3][j-1]=1;
-
-                    data[i-1][j+3]=1;
-                    data[i+1][j-3]=1;
-
-                    data[i+1][j+3]=1;
-                    data[i-1][j-3]=1;
-
                 }
             }
         }
 
 
-        for (int i=0; i<data.length; i++) {
-            System.out.println("");
+        // for (int i=0; i<data.length; i++) {
+        //     System.out.println("");
 
-            for (int j=0; j<data[0].length; j++) {
-                System.out.print(data[i][j]);
-                System.out.print(" ");
+        //     for (int j=0; j<data[0].length; j++) {
+        //         System.out.print(data[i][j]);
+        //         System.out.print(" ");
 
 
-            }
-        }
+        //     }
+        // }
 
         System.out.println("Total number of runs: " + counter);
         System.out.println("Successful runs: " + (counter-stuck));
