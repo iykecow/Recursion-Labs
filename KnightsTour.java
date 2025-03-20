2b1bef3ca7664f6d50e594a54f40c5b895d80f65
iykecow <142448965+iykecow@users.noreply.github.com>
2025-03-20 12:03:47 -0700

    f

--- KnightsTour.java
+++ KnightsTour.java
@@ -10,11 +10,11 @@ public class KnightsTour {
 
     private void knightMove(int [][] arr, int a, int x, int y, int left, int up) {
 
-        System.out.println("COUNTER IS " + counter);
+        // System.out.println("COUNTER IS " + counter);
         // System.out.println("xDim IS " + xDim);
         // System.out.println("yDim IS " + yDim);
 
-        if (counter==(xDim*yDim)-1) {
+        if (counter==63) {
             System.out.println("YES");
             finish++;
             return;
@@ -22,27 +22,32 @@ public class KnightsTour {
 
         if (x+left>xDim-1) {
             counter++;
+            System.out.println("HERE");
             stuck++;
             return;
         }
         else if (y+up>yDim-1) {
             counter++;
+            System.out.println("HERE");
             stuck++;
             return;
         }
         else if (y+up<0) {
             counter++;
+            System.out.println("HERE");
             stuck++;
             return;
         }
         else if (x+left<0) {
             counter++;
+            System.out.println("HERE");
             stuck++;
             return;
         }
 
         else if (arr[x+left][y+up]==10) {
             counter++;
+            System.out.println("HERE");
             stuck++;
             return;
         }
@@ -55,12 +60,72 @@ public class KnightsTour {
         }
     }
 
+
+
+
+    private int[][] knight_checker(int[][] data, int x, int y) {
+        int[][] storage = new int [8][2];
+        int tempstorage = 0;
+        for (int i=0; i>storage.length; i++) {
+        
+            if ((x+1) < 8 && (y-2) > 0) {
+                if (data[x+1][y-2]!=10) {
+                    tempstorage++;
+                }
+            }
+            if ((x-1) > 0 && (y+2) < 8) {
+                if (data[x-1][y+2]!=10) {
+                    tempstorage++;
+                }
+            }
+            if ((x+1) < 8 && (y+2) < 8) {
+                if (data[x+1][y+2]!=10) {
+                    tempstorage++;
+                }
+            }
+            if ((x-1) > 0 && (y-2) > 0) {
+                if (data[x-1][y-2]!=10) {
+                    tempstorage++;
+                }
+            }
+            if ((x+2) < 8 && (y-1) > 0) {
+                if (data[x+2][y-1]!=10) {
+                    tempstorage++;
+                }
+            }
+            if ((x-2) > 0 && (y+1) < 8) {
+                if (data[x-2][y+1]!=10) {
+                    tempstorage++;
+                }
+            }
+            if ((x+2) < 8 && (y+1) < 8) {
+                if (data[x+2][y+1]!=10) {
+                    tempstorage++;
+                }
+            }
+            if ((x-2) > 0 && (y-1) > 0) {
+                if (data[x-2][y-1]!=10) {
+                    tempstorage++;
+                }
+            }
+
+            storage[i][1] = tempstorage;
+            tempstorage = 0;
+        }
+
+        return storage;
+
+    }
+
     public KnightsTour(int [][] data)
     {
         board = data;
 
         xDim = board.length;
         yDim = board[0].length;
+
+        
+
 
 
         // Notes: 
@@ -72,42 +137,87 @@ public class KnightsTour {
         if (counter==0) {
             data[4][4] = 9;
         }
+        int[][] ashrag = knight_checker(data, 0,0);
+
+        int smallest = 100;
+        int currentsmallest = 9;
 
         for (int i=0; i<data.length; i++) {
             for (int j=0; j<data[0].length; j++) {
-
                 if (data[i][j]==9) {
-                    for (int k=0; k<8; k++) {
-                        if (k==0) {
-                            knightMove(data,k,i,j,3,-1);
-                        }
-                        if (k==1) {
-                            knightMove(data,k,i,j,-3,1);
-                        }
-                        if (k==2) {
-                            knightMove(data,k,i,j,3,1);
-                        }
-                        if (k==3) {
-                            knightMove(data,k,i,j,-3,-1);
-                        }
-                        if (k==4) {
-                            knightMove(data,k,i,j,1,-3);
-                        }
-                        if (k==5) {
-                            knightMove(data,k,i,j,-1,3);
-                        }
-                        if (k==6) {
-                            knightMove(data,k,i,j,1,3);
-                        }
-                        if (k==7) {
-                            knightMove(data,k,i,j,-1,-3);
-                        }
+                    data[i][j]++;
+                    int[][] tempcheck = knight_checker(data,i,j);
 
-                        // System.out.println("DONE");
-                    }
+                    
+                    for (int ii=0; ii<tempcheck.length; ii++) {
+                        if (tempcheck[i][2]<smallest&&tempcheck[i][j]!=0) {
+                            smallest = tempcheck[i][2];
+                            currentsmallest = tempcheck[i][1];
+                        }
                 }
             }
         }
+
+        // 0 0 0 0 0 0 0 0
+        // 0 0 0 0 0 0 0 0
+        // 0 0 2 0 3 0 0 0
+        // 0 6 0 0 0 7 0 0
+        // 0 0 0 9 0 0 0 0
+        // 0 8 0 0 0 5 0 0
+        // 0 0 4 0 1 0 0 0
+        // 0 0 0 0 0 0 0 0
+        // 0 0 0 0 0 0 0 0
+
+
+            if (currentsmallest==1) {
+                data[i+1][j-2]=9;
+            }
+            if (currentsmallest==2) {
+                data[i-1][j+2]=9;
+            }
+            if (currentsmallest==3) {
+                data[i+1][j+2]=9;
+            }
+            if (currentsmallest==4) {
+                data[i-1][j-2]=9;
+            }
+            if (currentsmallest==5) {
+                data[i+1][j-2]=9;
+            }
+
+
+        //         if (data[i][j]==9) {
+        //             for (int k=0; k<8; k++) {
+        //                 if (k==0) {
+        //                     knightMove(data,k,i,j,2,-1);
+        //                 }
+        //                 if (k==1) {
+        //                     knightMove(data,k,i,j,-2,1);
+        //                 }
+        //                 if (k==2) {
+        //                     knightMove(data,k,i,j,2,1);
+        //                 }
+        //                 if (k==3) {
+        //                     knightMove(data,k,i,j,-2,-1);
+        //                 }
+        //                 if (k==4) {
+        //                     knightMove(data,k,i,j,1,-2);
+        //                 }
+        //                 if (k==5) {
+        //                     knightMove(data,k,i,j,-1,2);
+        //                 }
+        //                 if (k==6) {
+        //                     knightMove(data,k,i,j,1,2);
+        //                 }
+        //                 if (k==7) {
+        //                     knightMove(data,k,i,j,-1,-2);
+        //                 }
+
+        //                 // System.out.println("DONE");
+        //             }
+        //         }
+        //     }
+        // }
 
 
         // for (int i=0; i<data.length; i++) {
@@ -121,9 +231,9 @@ public class KnightsTour {
         //     }
         // }
 
-        System.out.println("Total number of runs: " + counter);
-        System.out.println("Successful runs: " + (counter-stuck));
-        System.out.println("Failed Runs: " + stuck);
+        // System.out.println("Total number of runs: " + counter);
+        // System.out.println("Successful runs: " + (counter-stuck));
+        // System.out.println("Failed Runs: " + stuck);
 
     }
     
--- Main.java
+++ Main.java
@@ -4,32 +4,33 @@ public class Main
 {
     public static void main(String [] args)
     {
-        System.out.println("Please enter board dimensions: ");
-        System.out.println("Please note that it is impossible to solve the Knight's tour for boards of less than 5x5");
+        // System.out.println("Please enter board dimensions: ");
+        // System.out.println("Please note that it is impossible to solve the Knight's tour for boards of less than 5x5");
 
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
 
-        System.out.println("X: ");
-        String choiceX = in.nextLine();
-        while(Integer.parseInt(choiceX) <= 4)
-        {
-            System.out.println("Invalid");
-            choiceX = in.nextLine();
-        }
+        // System.out.println("X: ");
+        // String choiceX = in.nextLine();
+        // while(Integer.parseInt(choiceX) <= 4)
+        // {
+        //     System.out.println("Invalid");
+        //     choiceX = in.nextLine();
+        // }
 
-        System.out.println("Y: ");
-        String choiceY = in.nextLine();
-        while(Integer.parseInt(choiceY) <= 4)
-        {
-            System.out.println("Invalid");
-            choiceY = in.nextLine();
-        }
+        // System.out.println("Y: ");
+        // String choiceY = in.nextLine();
+        // while(Integer.parseInt(choiceY) <= 4)
+        // {
+        //     System.out.println("Invalid");
+        //     choiceY = in.nextLine();
+        // }
         // System.out.println(Integer.parseInt(choiceX));
         // System.out.println(Integer.parseInt(choiceY));
 
-        int [][] data = new int[Integer.parseInt(choiceX)][Integer.parseInt(choiceY)];
-        
+        int [][] data = new int[8][8];
         
         new KnightsTour(data);
+
+        
     }
 }
